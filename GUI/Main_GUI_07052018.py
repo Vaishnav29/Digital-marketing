3685554532eb0826b5e54c11acda3373903889fc
Vaishnav29 <37918765+Vaishnav29@users.noreply.github.com>
2018-05-16 14:21:23 +0530

    BG color changes

--- GUI/Main_GUI_07052018.py
+++ GUI/Main_GUI_07052018.py
@@ -3,11 +3,16 @@ import os
 root = Tk()
 root.title('Digital Marketing')
 root.geometry("900x600")
-root.config(background = '#EEC774')
-label = Label(root, borderwidth= 10, background= '#B0E5B9', text='Welcome to Digital Marketing Portal!', font=36)
+root.config(background = '#7E85AB')
+root.resizable(0,0)
+label = Label(root, borderwidth= 10, background= '#7E85AB', text='Welcome to Digital Marketing Portal!', font=36)
 label.place(relx=0.4,rely=0.05)
+
+    
 def inventoryManagement():
     os.system('python inventory_gui.py')
+    print("hello")
+    
     
 def targetmarketing():
     os.system('python Targetfinal.py')
@@ -15,21 +20,24 @@ def targetmarketing():
 def personalised():
     os.system('python personalised_gui.py')
 
-inventory = Button(root, background = '#759EBD',text = "Inventory Managemnet", padx = 50, pady = 30, wraplength=80, command=inventoryManagement)
+#style = ttk.Style()
+#style.configure("BW.TLabel", foreground="black", background="white")
+
+inventory = Button(root,foreground="black", text = "Inventory Managemnet", padx = 50, pady = 30, wraplength=80, command=inventoryManagement)
 inventory.place(relx = 0.1,rely = 0.15)
-pricing = Button(root, background = '#759EBD',text = "Pricing Strategy", padx = 50, pady = 30, wraplength=80)
+pricing = Button(root,text = "Pricing Strategy", padx = 50, pady = 30, wraplength=80)
 pricing.place(relx= 0.45,rely=0.15)
-customer = Button(root, background = '#759EBD',text = "Customer Segmentation", padx = 50, pady = 30, wraplength=80)
+customer = Button(root, text = "Customer Segmentation", padx = 50, pady = 30, wraplength=80)
 customer.place(relx= 0.75,rely=0.15)
-target = Button(root, background = '#759EBD',text = "Targert Marketing", padx = 50, pady = 30, wraplength=80, command = targetmarketing)
+target = Button(root, text = "Targert Marketing", padx = 50, pady = 30, wraplength=80, command = targetmarketing)
 target.place(relx= 0.1,rely=0.455)
 #smart = Button(root, background = '#759EBD',text = "Smart Advertising", padx = 50, pady = 30, wraplength=80)
 #smart.place(relx= 0.45,rely=0.455)
-personalized = Button(root, background = '#759EBD',text = "Personalized Experience", padx = 50, pady = 30, wraplength=80, command = personalised)
+personalized = Button(root, text = "Personalized Experience", padx = 50, pady = 30, wraplength=80, command = personalised)
 personalized.place(relx= 0.45,rely=0.455)
 def openBokeh():
     os.system('bokeh serve --show C:/Users/user/Desktop/dashboard')
-visualization = Button(root, background = '#759EBD',text = "Visualization", padx = 50, pady = 30, wraplength=80, command=openBokeh)
+visualization = Button(root, text = "Visualization", padx = 50, pady = 30, wraplength=80, command=openBokeh)
 visualization.place(relx= 0.75,rely=0.455)
 
 root.mainloop()
