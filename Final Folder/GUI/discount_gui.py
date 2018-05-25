cec5d396729ba8a9bde6f9ef3f95ccf75c522a14
Vaishnav29 <37918765+Vaishnav29@users.noreply.github.com>
2018-05-25 10:38:20 +0530

    Update discount_gui.py

--- Final Folder/GUI/discount_gui.py
+++ Final Folder/GUI/discount_gui.py
@@ -1,3 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 25 10:11:11 2018
+
+@author: user
+"""
+
 from tkinter import *
 import pandas as pd
 from math import sqrt
@@ -20,104 +27,95 @@ discount = pickle.load(open('dicount.p','rb'))
 root = Tk()
 root.title('Pricing strategies')
 root.geometry('900x600')
-root.configure(background='RoyalBlue3')
 
-Label(root, text = 'Products eligible for giving discounts', font =('Helvetica', 15, 'bold', 'underline')).place(relx = 0.25, rely = 0.04)
+Label(root, text = 'Products eligible for giving discounts', font =('Helvetica', 15, 'bold', 'underline')).place(relx = 0.3, rely = 0)
 
 def getprod():
     print(sub.get(),sub1.get())
     print(int(sub1.get())-3)
     
-    #The Party Starts Here 
+    #Setting year and month values
+    global year 
+    year= int(sub.get())
+    global month 
+    month=int(sub1.get())
+    print(year,month)
     
-    f=sub.get()
+    #Cases for Jan, Feb and March
+    if month==3:
+        month= 12
+        year-=1
+    elif month==2:
+        month= 11
+        year-=1
+    elif month==1:
+        month=10
+        year-=1
+    print(year,month)
     
-    g= sub1.get()
-    global a 
-    a= int(f)
-    global b 
-    b=int(g)
-    print(a,b)
-    if b==3:
-        b= 12
-        a-=1
-    elif b==2:
-        b= 11
-        a-=1
-    elif b==1:
-        b=10
-        a-=1
-    print(a,b)
+    #Loading Data from Pickle file
     global data
     global dict1    
     data= pickle.load(open('logdata.p','rb'))  
     dict1= pickle.load(open('logdict.p','rb'))  
+    
+    #Assigning year and month value for the test dataset
     if sub1.get()==1 or sub1.get()==2 or sub1.get()==3:
         
-        y = pd.DatetimeIndex(data['Order Date']).year == int(a)
-        m = pd.DatetimeIndex(data['Order Date']).month== int(b)
+        y = pd.DatetimeIndex(data['Order Date']).year == int(year)
+        m = pd.DatetimeIndex(data['Order Date']).month== int(month)
     
     else:
-        y = pd.DatetimeIndex(data['Order Date']).year == int(a)
-        m = pd.DatetimeIndex(data['Order Date']).month== int(b)-3
+        y = pd.DatetimeIndex(data['Order Date']).year == int(year)
+        m = pd.DatetimeIndex(data['Order Date']).month== int(month)-3
+    
+    #Get the index of the last row of the train dataset
     i=0
-    p=0
+    limit=0  
     for i in range(len(data)):
         if(y[i]== True):
             if(m[i]==True):
-                p=i
+                limit=i
                 break
                   
             
             
+    #Get train data features and Target
+    train_data= data[:limit][['Customer ID','Region','Product I','Sub-Category','Age','Profit','Quantity']]
+    discount_data = data[:limit]['Discount']
     
-    test_data= data[:p][['Customer ID','Region','Product I','Sub-Category','Age','Profit','Quantity']]
-    discount_data = data[:p]['Discount']
-    
-    
+    #Implementing Logistic Regression on the train dataset
     logreg = LogisticRegression()
-    logreg.fit(test_data, discount_data)
+    logreg.fit(train_data, discount_data)
     
     
-    
+    #Predicting values for the Discount (Using test dataset)
     global y_pred
     y_pred=[]
-    #= y_test.reset_index(drop=True)
     global dataset
-    dataset= data[p:][['Customer ID','Region','Product I','Sub-Category','Age','Profit','Quantity']]
+    dataset= data[limit:][['Customer ID','Region','Product I','Sub-Category','Age','Profit','Quantity']]
     print(dataset)
     dataset = dataset.reset_index(drop = True)
     print(dataset)
     y_pred= logreg.predict(dataset)
-    
     print(y_pred)
-    
     i=0
     j=0
     global D
     D = []
 
-# =============================================================================
-#     while(i< len(y_pred)):
-#          if(y_pred[i] == 1):
-#              D[j] = data['Product I'][i]
-#              
-#              j+=1
-#          i+=1    
-#              
-#     print(D)   
-#     
-# =============================================================================
+    #Get all product id's that are eligible for discounts
     for i in range(len(dataset)):
         if(y_pred[i] == 1):
             
             D.append(dataset['Product I'][i])
-            #print(dataset['Product I'][i],data['Product I'][i])
     print(D)       
     D= list(set(D))
     h=[]
+    
+    #get all product names that ar eligible for discounts
     for i in range(len(D)):
-        h.append(dict1[D[i]])
+        h.append(dict1[D[i]]) #gets product name based on the product id and then is stored into a list
             
     
     j = 1
@@ -126,12 +124,12 @@ def getprod():
         lis.insert(END,'{}. {}'.format(j, h[i]))
         j +=1
         
-        
+    #Testing accuracy of the model using Cross-Validation
     kfold = model_selection.KFold(n_splits=10, random_state=7)
     modelCV = LogisticRegression()
     scoring = 'accuracy'
-    X_train = data[:p][['Customer ID','Region','Product I','Sub-Category','Age','Profit','Quantity']]
-    y_train = data[:p]['Discount']
+    X_train = data[:limit][['Customer ID','Region','Product I','Sub-Category','Age','Profit','Quantity']]
+    y_train = data[:limit]['Discount']
     results = model_selection.cross_val_score(modelCV, X_train, y_train, cv=kfold, scoring=scoring)
     print("10-fold cross validation average accuracy: %.3f" % (results.mean()))
 
@@ -155,5 +153,18 @@ sub1.set(month[0])
 sub1Dropdown = OptionMenu(root,sub1,*month)
 #subDropdown.configure(fg = 'black')
 sub1Dropdown.place(relx = 0.745, rely = 0.08)
+
+
+
+#The Party Starts Here 
+
+
+
+
+
+
+
+
+
 
 root.mainloop()
